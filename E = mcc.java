0f062c566a01963d64ccedd55484f6eb2759fce7
Borags <133859621+Borags@users.noreply.github.com>
2023-08-27 17:54:47 +0800

    Updated

--- E = mcc.java
+++ E = mcc.java
@@ -3,23 +3,33 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-
 public class Emcc {
     public static void main(String[] args) {
         Scanner sc;
         BigDecimal m;
         BigDecimal c;
-        String e;
+        BigDecimal e;
         NumberFormat a;
+        String b;
         
-        sc = new Scanner(System.in);
-                
-        System.out.print("Mass = ");
+        
+        sc = new Scanner(System.in); // New Scanner
+        
+        c = new BigDecimal("299792458"); // Speed of Light
+        System.out.print("Mass = "); // Mass(KG) & Number only Input
+        while (!sc.hasNextDouble()) {
+            System.out.println("Must be a Number."); 
+            sc.next();
+        }
         m = sc.nextBigDecimal();
-        c = new BigDecimal("299792458");
-        a = NumberFormat.getNumberInstance(Locale.US);
-        e = a.format(m.multiply(c.pow(2)));
         
-        System.out.println("Energy= " + e + " Joules");
+        e = m.multiply(c.pow(2)); // Energy
+        
+        a = NumberFormat.getNumberInstance(Locale.US); // Formating
+        b = a.format(e);
+        
+        System.out.println("Energy= " + b + " Joules"); // Printing Formatted Number
+        
+        sc.close();
     }
 }
