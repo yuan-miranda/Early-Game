8faee69fb95bbfa898482426eb0539c10e1e096d
Borags <133859621+Borags@users.noreply.github.com>
2023-09-03 15:42:36 +0800

    Update Pythagorian_GUI.java
    compacted

--- Pythagorian_GUI.java
+++ Pythagorian_GUI.java
@@ -1,71 +1,61 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
-public class PythagorianTheorem {
+public class PythagoreanTheorem {
     public static void main(String[] args) {
-        char input = readChar("What side are you looking for? A, B or C");
+        Scanner sc = new Scanner(System.in);
 
-        double a;
-        double b;
-        double c;
-        
-        // Computation
-        if (input == 'a' || input == 'A') {
-            c = readDouble("What is your Hypotenuse / C = ");
-            b = readDouble("What is your Opposite / B = ");
+        System.out.print("Side to calculate (A, B, or C): ");
+        char side = gC(sc);
+        double a, b, c;
 
-            a = Math.sqrt((c * c) - (b * b));
-            JOptionPane.showMessageDialog(null, "Your Base is = " + a);
+        switch (Character.toLowerCase(side)) {
+            case 'a':
+                c = rD(sc, "Hypotenuse (C) = ");
+                b = rD(sc, "Opposite (B) = ");
+                a = Math.sqrt(c * c - b * b);
+                System.out.println("Base (A) = " + a);
+                break;
 
-            
-        } else if (input == 'b' || input == 'B') {
-            a = readDouble("What is your base / A = ");
-            c = readDouble("What is your Hypotenuse / C = ");
+            case 'b':
+                a = rD(sc, "Base (A) = ");
+                c = rD(sc, "Hypotenuse (C) = ");
+                b = Math.sqrt(a * a - c * c);
+                System.out.println("Opposite (B) = " + b);
+                break;
 
-            b = Math.sqrt((a * a) - (c * c));
-            JOptionPane.showMessageDialog(null, "Opposite is = " + b);
+            case 'c':
+                a = rD(sc, "Base (A) = ");
+                b = rD(sc, "Opposite (B) = ");
+                c = Math.sqrt(a * a + b * b);
+                System.out.println("Hypotenuse (C) = " + c);
+                break;
 
-            
-        } else if (input == 'c' || input == 'C') {
-            a = readDouble("What is your Base / A = ");
-            b = readDouble("What is your Opposite / B = ");
-
-            c = Math.sqrt((a * a) + (b * b));
-            JOptionPane.showMessageDialog(null, "Your Hypotenuse is = " + c);
-
-            
-        } else {
-            JOptionPane.showMessageDialog(null, "Here's your extra chromosomes... Retard.");
+            default:
+                System.out.println("Invalid input.");
         }
+
+        sc.close();
     }
 
-    // Only Triangle's Side
-    public static char readChar(String prompt) {
+    public static char gC(Scanner sc) {
         while (true) {
-            String input = JOptionPane.showInputDialog(prompt);
-            if (input != null && input.length() == 1) {
-                char character = input.charAt(0);
-                if (validChar(character)) {
-                    return character;
-                }
+            String inp = sc.nextLine().toLowerCase();
+            if (inp.length() == 1 && "abc".contains(inp)) {
+                return inp.charAt(0);
             }
-            JOptionPane.showMessageDialog(null, "Enter a side of a triangle.");
+            System.out.println("Enter a Side");
         }
     }
 
-    // Length of the Triangle's Side
-    public static double readDouble(String prompt) {
+    public static double rD(Scanner sc, String prompt) {
         while (true) {
-            String input = JOptionPane.showInputDialog(prompt);
+            System.out.print(prompt);
+            String inp = sc.nextLine();
             try {
-                return Double.parseDouble(input);
+                return Double.parseDouble(inp);
             } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Enter a number.");
+                System.out.println("Enter a number.");
             }
         }
-    }
-
-    // Allowed Char of Triangle's Side
-    public static boolean validChar(char character) {
-        return (character == 'a' || character == 'b' || character == 'c' || character == 'A' || character == 'B' || character == 'C');
     }
 }
